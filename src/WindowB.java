572fecce89c6d57cfb68fb2dae949eb93c4c31f4
Khritavarman <Khritavarman@gmail.com>
2016-06-14 14:38:20 +0300

    Second Button

--- src/WindowB.java
+++ src/WindowB.java
@@ -1,19 +1,37 @@
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by Kostyan on 14.06.2016.
  */
 public class WindowB extends JFrame {
 
-    FormB formB = null;
+    FormB formB = new FormB();
 
     public WindowB() {
         super("Window B");
         setSize(800,600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        formB = new FormB();
         setContentPane(formB.getRootPanel());
         setVisible(true);
+
+        formB.getButtonBtoA().addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                dispose();
+                String[] array = formB.getFio().getText().split("[\\s]+");
+                FormA formA = new WindowA().getFormA();
+                formA.getSurname().setText(array[0]);
+                formA.getName().setText(array[1]);
+                formA.getSecondName().setText(array[2]);
+            }
+        });
+    }
+
+    public FormB getFormB() {
+        return formB;
     }
 }
